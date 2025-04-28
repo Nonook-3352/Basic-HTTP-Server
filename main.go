c51060b1c441dd21b436aad847acddc8a9d45cf3
nonook <104591876+Nonook-3352@users.noreply.github.com>
2025-04-28 21:06:47 +0200

    Feat: Basic HTTP server functions, html rendering functions and working server

--- main.go
+++ main.go
@@ -2,8 +2,36 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"html/template"
+	"log"
 )
 
+var PORT int = 8080
+
+type MetaData struct {
+		Port int
+		Title string
+	}
+
+func RenderTemplate(w http.ResponseWriter, fileName string, data any) {
+	t, err := template.ParseFiles("Public/" + fileName + ".html")
+	if err != nil {
+		log.Println("[html/template]: ", err)
+	}
+	t.Execute(w, data)
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+	data := MetaData{Port: PORT, Title: "index"}
+	RenderTemplate(w, "index", data)
+}
+
 func main() {
-	fmt.Println("Hello World")
+    fmt.Printf("Starting HTTP server at localhost:%d\n", PORT)
+	
+	http.HandleFunc("/", root)
+	
+	port := fmt.Sprint(":", PORT) // Change the PORT variable from int to a string
+	log.Fatal(http.ListenAndServe(port, nil))
 }
